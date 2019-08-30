ea16dc9fd520763e16c74167c4db1cf2276f4a6e
jaldazabal <jon3aldazabal@gmail.com>
2019-08-30 09:46:24 +0200

    Stop using mapbox

--- js/model/MapGraph.js
+++ js/model/MapGraph.js
@@ -10,12 +10,10 @@ var MapGraph = (function() {
         $('#divGraph').html('<div id="divMap"></div>');
         $('#divMap').css('width','100%').css('height','100%');
         var europeMap = L.map('divMap').setView([54.5260,15.2551],3);
-        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}',{
-            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
             maxZoom: 5,
-            minZoom: 3,
-            id: 'mapbox.light',
-            accessToken: 'pk.eyJ1IjoiamFsZGF6YWJhbCIsImEiOiJjanppZWhsZ20wODMxM21vOGs4MHV2Y2toIn0.O8f9E4GyBreItjEADNYGcQ'
+            minZoom: 3
         }).addTo(europeMap);
         return europeMap;
     }
