a4b0f9749007c11c30852d5ab0c8d66ef6c382d7
Jon Aldazabal <jon3aldazabal@gmail.com>
2020-02-11 20:35:00 +0100

    Called new data access methods in every graphic type

--- js/model/Graphic.js
+++ js/model/Graphic.js
@@ -2,15 +2,17 @@ var Graphic = (function () {
     async function chooseGraphicToDraw(graphicOptions) {
         $('#divGraph').empty();
         $('#divGraph').removeClass('c3');
+        let geoJSONdata;
         switch (graphicOptions.type) {
             case 'mapGraphic':
                 graphicOptions.year = $('#dataYear').children('option:selected').val();
-                const geoJSONdata = await dataAccess(graphicOptions);
+                geoJSONdata = await dataAccess(graphicOptions);
                 MapGraph.generateMap(geoJSONdata, graphicOptions);
                 break;
             case 'rankGraphic':
                 graphicOptions.year = $('#dataYear').children('option:selected').val();
                 graphicOptions.component = $('#rankComponent').children('option:selected').val();
+                geoJSONdata = await dataAccess(graphicOptions);
                 if (graphicOptions.component !== 'Total (100)') {
                     $('#breakDownCheckRank').prop('checked',false);
                     $('#breakDownCheckRank').prop('disabled',true);
@@ -18,9 +20,11 @@ var Graphic = (function () {
                     $('#breakDownCheckRank').prop('disabled',false);
                     graphicOptions.breakDownScoresRank = document.getElementById('breakDownCheckRank').checked;
                 }
-                RankingGraph.generateGraph(geoJSONdata, graphicOptions);
+                console.log(graphicOptions);
+                RankingGraph.generateGraph(graphicOptions);
                 break;
             case 'evoGraphic':
+                geoJSONdata = await dataAccess(graphicOptions);
                 graphicOptions.component = $('#typeEvolution').children('option:selected').val();
                 graphicOptions.arrayCountries = $('#countryMultiInput').val().split(',');
                 EvolutionGraph.generateEvoGraph(geoJSONdata, graphicOptions);
@@ -30,7 +34,7 @@ var Graphic = (function () {
         }
     }
 
-    async function dataAccess(graphicOptions) {
+    function dataAccess(graphicOptions) {
         if(graphicOptions.type === 'mapGraphic' || graphicOptions.type === 'rankGraphic') {
             return DataAccess.singleYearGeoJSON(graphicOptions.year);
         } else {
